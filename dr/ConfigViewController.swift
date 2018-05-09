4259d815e394675772e4a13bf739124f8f6691da
Kazuo Tsubaki <kazz12211@gmail.com>
2018-05-09 23:17:41 +0900

    Fixed #8

--- dr/ConfigViewController.swift
+++ dr/ConfigViewController.swift
@@ -31,29 +31,36 @@ class ConfigViewController: UIViewController {
         frameRateSegmentedControl.selectedSegmentIndex = Config.default.availableFrameRates.index(of: Config.default.frameRate)!
         recordAudioSwitch.setOn(Config.default.recordAudio, animated: false)
         autoStartSpeedField.text = "".appendingFormat("%.0f", Config.default.autoStartSpeed)
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(contentViewTapped)))
     }
 
     @IBAction func autoStartSwitched(_ sender: UISwitch) {
+        contentViewTapped()
         Config.default.autoStartEnabled = sender.isOn
     }
     
     @IBAction func autoStopSwitched(_ sender: UISwitch) {
+        contentViewTapped()
         Config.default.autoStopEnabled = sender.isOn
     }
     
     @IBAction func gsensorChanged(_ sender: UISegmentedControl) {
+        contentViewTapped()
         Config.default.gsensorSensibility = Config.default.availableGSensorSensibilities[sender.selectedSegmentIndex]
     }
     
     @IBAction func videoQualityChanged(_ sender: UISegmentedControl) {
+        contentViewTapped()
         Config.default.videoQuality = Config.default.availableVideoQualities[sender.selectedSegmentIndex]
     }
     
     @IBAction func frameRateChanged(_ sender: UISegmentedControl) {
+        contentViewTapped()
         Config.default.frameRate = Config.default.availableFrameRates[sender.selectedSegmentIndex]
     }
     
     @IBAction func recordAudioSwitched(_ sender: UISwitch) {
+        contentViewTapped()
         Config.default.recordAudio = sender.isOn
     }
     
@@ -68,6 +75,15 @@ class ConfigViewController: UIViewController {
     @IBAction func dismiss(_ sender: Any) {
         Config.default.save()
         dismiss(animated: true) {
+        }
+    }
+}
+
+extension ConfigViewController {
+    
+    @objc private func contentViewTapped() {
+        if autoStartSpeedField.isFirstResponder {
+            autoStartSpeedField.resignFirstResponder()
         }
     }
 }
